7df18bf983aa09f9be7ec4cf2ce77466f80fccea
Tyllllll <874026556@qq.com>
2019-02-01 13:38:01 +0800

    Signed-off-by: Tyllllll <874026556@qq.com>

--- identify.c
+++ identify.c
@@ -20,8 +20,8 @@ void Find_Line(void)
 			//寻找左跳变点
 			if(camera.image[i][j] != 0 && camera.image[i][j - 2] == 0)
 			{
-				line.leftline[i] = j - 1;
-				line.leftlineflag[i] = 1;
+				line.left_line[i] = j - 1;
+				line.left_line_flag[i] = 1;
 				break;
 			}
 		}
@@ -31,30 +31,30 @@ void Find_Line(void)
 			//寻找右跳变点
 			if(camera.image[i][j] != 0 && camera.image[i][j + 2] == 0)
 			{
-				line.rightline[i] = j + 1;
-				line.rightlineflag[i] = 1;
+				line.right_line[i] = j + 1;
+				line.right_line_flag[i] = 1;
 				break;
 			}
 		}
 		//计算中线
-		if(line.leftlineflag[i] && line.rightlineflag[i])
+		if(line.left_line_flag[i] && line.right_line_flag[i])
 		{
-			line.midline[i] = (line.leftline[i] + line.rightline[i]) / 2;
+			line.midline[i] = (line.left_line[i] + line.right_line[i]) / 2;
 		}
-		else if(line.leftlineflag[i] && !line.rightlineflag[i])
+		else if(line.left_line_flag[i] && !line.right_line_flag[i])
 		{
-			line.rightline[i] = 159;
-			line.midline[i] = (line.leftline[i] + line.rightline[i]) / 2;
+			line.right_line[i] = 159;
+			line.midline[i] = (line.left_line[i] + line.right_line[i]) / 2;
 		}
-		else if(!line.leftlineflag[i] && line.rightlineflag[i])
+		else if(!line.left_line_flag[i] && line.right_line_flag[i])
 		{
-			line.leftline[i] = 0;
-			line.midline[i] = line.rightline[i] / 2;
+			line.left_line[i] = 0;
+			line.midline[i] = line.right_line[i] / 2;
 		}
-		else if(!line.leftlineflag[i] && !line.rightlineflag[i])
+		else if(!line.left_line_flag[i] && !line.right_line_flag[i])
 		{
-			line.leftline[i] = 0;
-			line.rightline[i] = 159;
+			line.left_line[i] = 0;
+			line.right_line[i] = 159;
 			if(100 == i)
 			{
 				line.midline[i] = 80;
@@ -82,7 +82,7 @@ void Judge_Feature(void)
 /***************************************************************
 	*	@brief	找顶点
 	*	@param	无
-	*	@note	无
+	*	@note	顶点的定义为某一行的中线上的为白色点，但是该中线列的后面的点为黑点，即赛道边界
 ***************************************************************/
 void Find_Toppoint(void)
 {
@@ -112,16 +112,37 @@ void Find_Toppoint(void)
 /***************************************************************
 	*	@brief	找拐点
 	*	@param	无
-	*	@note	无
+	*	@note	十字近点处拐点（透视图中最大最小处）
 ***************************************************************/
 void Find_Inflection(void)
 {
 	uint8 i = 0;
-	if(feature.toppoint < 70 && feature.toppoint > 5)
+	if(feature.toppoint < 100 && feature.toppoint > 20)
 	{
 		for(i = 108; i > feature.toppoint + 4; i--)
 		{
-			
+			if(feature.left_flection_flag == 0)
+			{
+				//位置条件
+				if(i > 35 && line.left_line[i] < 140)
+				{
+					//大小条件
+					if(line.left_line[i] > line.left_line[i + 3] && line.left_line[i] > line.left_line[i - 3] && line.left_line[i] > line.left_line[i + 5] &&
+					   line.left_line[i] > line.left_line[i - 2] && line.left_line[i] > line.left_line[i - 4] && line.left_line[i - 3] - line.left_line[i + 3] < 4)
+					{
+						//存在性条件
+						if(line.left_line_flag[i + 3] == 1 && line.left_line_flag[i + 5] == 1 && line.left_line_flag[i] == 1)
+						{
+							//斜率条件
+							if(line.left_line[i] - line.left_line[i - 3] - line.left_line[i + 3] + line.left_line[i] > 4)
+							{
+								feature.left_flection_row = i;
+								feature.left_flection_flag = 1;
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
--- identify.h
+++ identify.h
@@ -3,10 +3,10 @@
 
 typedef struct
 {
-	uint8 leftline[120];
-	uint8 leftlineflag[120];
-	uint8 rightline[120];
-	uint8 rightlineflag[120];
+	uint8 left_line[120];
+	uint8 left_line_flag[120];
+	uint8 right_line[120];
+	uint8 right_line_flag[120];
 	uint8 midline[120];
 }Line_Class;
 extern Line_Class line;
@@ -14,8 +14,10 @@ extern Line_Class line;
 typedef struct
 {
 	uint8 toppoint;
-	uint8 leftflection;
-	uint8 rightflection;
+	uint8 left_flection_row;
+	uint8 left_flection_flag;
+	uint8 right_flection_row;
+	uint8 right_flection_flag;
 }Feature_Class;
 extern Feature_Class feature;
 
